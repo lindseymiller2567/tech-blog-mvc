897e4f0ba30c61e8f4ebcd14f3c2b3298aedf22d
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-03-05 12:07:13 -0600

    added more api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -33,14 +33,79 @@ router.get('/', (req, res) => {
         });
 });
 
-// POST // localhost:3001/api/posts/
+// GET // localhost:3001/api/posts/:id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST create new post // localhost:3001/api/posts/
 router.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
         post_content: req.body.post_content,
-        user_id: req.body.user_id // will need to change this to: req.session.user_id // grabs user id from the session instead of body
+        user_id: req.session.user_id // will need to change this to: req.session.user_id // grabs user id from the session instead of body
     })
         .then(dbPostData => {
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT or update the post's title
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
             res.json(dbPostData);
         })
         .catch(err => {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
         });
 });
 
-// GET // localhost:3001/api/users/:id
+// GET // localhost:3001/api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
@@ -41,6 +41,7 @@ router.get('/:id', (req, res) => {
                 res.status(404).json({ message: 'No user found with this id' });
                 return;
             }
+            res.json(dbUserData);
         })
         .catch(err => {
             console.log(err);
@@ -48,7 +49,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
-// POST // localhost:3001/api/users/
+// POST create a user // localhost:3001/api/users/
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
@@ -62,6 +63,29 @@ router.post('/', (req, res) => {
 
                 res.json(dbUserData);
             })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT update user username or password // localhost:3001/api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', password: 'password1234'}
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead 
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
         })
         .catch(err => {
             console.log(err);
